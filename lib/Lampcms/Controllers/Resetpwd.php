fc3e1f6d4e496523991b07314d2e55d415bb83eb
Boban <nbb.hss@gmail.com>
2012-03-30 13:41:53 +0530

    Password reset issue

--- lib/Lampcms/Controllers/Resetpwd.php
+++ lib/Lampcms/Controllers/Resetpwd.php
@@ -72,7 +72,7 @@ class Resetpwd extends WebPage
 	const EMAIL_BODY = 'This email contains your login information for %1$s
 
 Your login: %2$s
-Your password is: (hidden)
+Your password is: %3$s
 
 You are advised to store the above information in a safe place so that you
 do not face any inconvenience in future.
